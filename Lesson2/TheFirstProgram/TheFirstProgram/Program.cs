7a0c94e52c707a45e6142f8015b4dbf1dc89c1bf
AndreyDavydov1992 <petrioramis@gmail.com>
2022-04-25 00:57:49 +0700

    initial commit

---
+++ Lesson2/TheFirstProgram/TheFirstProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TheFirstProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //вольная интерпретация на тему Hello World
+            Console.WriteLine("Wake up Neo");
+            Console.WriteLine("The Matrix has you...");
+            Console.WriteLine("Follow the white rabbit...");
+            Console.WriteLine("Knock knock Neo.");
+            string input = System.Console.ReadLine();
+            Console.Clear();    
+        }
+    }
+}
